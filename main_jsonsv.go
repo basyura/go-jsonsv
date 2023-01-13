afc98b5f4d2a1a68b73c76dea97536e860b29c0f
basyura <basyura@gmail.com>
2023-01-13 22:39:17 +0900

    format

--- main_jsonsv.go
+++ main_jsonsv.go
@@ -11,37 +11,37 @@ import (
 )
 
 func main() {
-    port := "1323"
+	port := "1323"
 
-    args :=  os.Args
-    if len(args) > 1 {
-        port = args[1]
-    }
+	args := os.Args
+	if len(args) > 1 {
+		port = args[1]
+	}
 
-    exe, err:=  os.Executable()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    dir := filepath.Dir(exe)
-    path :=  filepath.Join(dir, "res.json")
-    _, err =  os.Stat(path)
-    if err != nil {
-        fmt.Println("response file not found :", path)
-        return
-    } else {
-        fmt.Println(path)
-    }
+	exe, err := os.Executable()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	dir := filepath.Dir(exe)
+	path := filepath.Join(dir, "res.json")
+	_, err = os.Stat(path)
+	if err != nil {
+		fmt.Println("response file not found :", path)
+		return
+	} else {
+		fmt.Println(path)
+	}
 
-    bytes, err := ioutil.ReadFile(path)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	e := echo.New()
 	e.POST("/", func(c echo.Context) error {
-        return c.Blob(http.StatusOK, echo.MIMEApplicationJSONCharsetUTF8, bytes)
+		return c.Blob(http.StatusOK, echo.MIMEApplicationJSONCharsetUTF8, bytes)
 	})
 	e.Logger.Fatal(e.Start(":" + port))
 }
